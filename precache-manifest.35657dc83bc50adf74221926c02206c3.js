89b375e936be7f6e0bd721f7e205daef56cd77b3
Code Sandbox <hello@codesandbox.io>
2021-09-03 09:38:36 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.35657dc83bc50adf74221926c02206c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d08ce1c5763c0ac901384baa790fcbb",
+    "url": "https://vaibhav18matere.github.io/csb-ionfv/index.html"
+  },
+  {
+    "revision": "7c8d4e3754e5488b784b",
+    "url": "https://vaibhav18matere.github.io/csb-ionfv/static/css/main.92b20f2a.chunk.css"
+  },
+  {
+    "revision": "4237d2ca33e90c2d6d67",
+    "url": "https://vaibhav18matere.github.io/csb-ionfv/static/js/2.d0a5e18a.chunk.js"
+  },
+  {
+    "revision": "464fef3691779b3926634327df24dae8",
+    "url": "https://vaibhav18matere.github.io/csb-ionfv/static/js/2.d0a5e18a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c8d4e3754e5488b784b",
+    "url": "https://vaibhav18matere.github.io/csb-ionfv/static/js/main.6708e46d.chunk.js"
+  },
+  {
+    "revision": "23e409b684e350375a61",
+    "url": "https://vaibhav18matere.github.io/csb-ionfv/static/js/runtime-main.cfb4648b.js"
+  }
+]);
